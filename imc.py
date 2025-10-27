c750f24073c24d304ad4716f9082e8dbee974925
Luiz Machi Lozano <lozano84dev@gmail.com>
2025-10-27 11:12:33 -0300

    criação da função classificarImc

--- imc.py
+++ imc.py
@@ -2,4 +2,13 @@ def calcularImc(peso, altura):
     imc = peso / (altura * altura)
     return imc
 
-print(calcularImc(95, 1.80))
+def classificarImc(imc):
+    if(imc < 18.5):
+        print('Abaixo do Peso.')
+    else:
+        print('peso normal')
+
+imc = calcularImc(95, 1.80)
+print(imc)
+classificarImc(imc)
+
